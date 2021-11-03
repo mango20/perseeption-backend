35343a4cd574f3ca484318872d6b3c94c452904f
unknown <mangacharmainelilan20@gmail.com>
2021-11-04 01:56:58 +0800

    make it better

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ cloudinary.config({
 app.post("/uploadEventImage", (req, res) => {
   // const file = req.file.filename;
   const announcement_details = {
-    EVENT_IMAGE: req.file.filename,
+    EVENT_IMAGE: req.body.filename,
     EVENT_TITLE: "req.body.title",
     EVENT_CONTENT: "req.body.content",
   };
