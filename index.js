b5afb301344b039dcaf931c644eaa2f35e08bcff
unknown <mangacharmainelilan20@gmail.com>
2021-11-03 22:14:11 +0800

    make it better

--- index.js
+++ index.js
@@ -390,6 +390,21 @@ app.post("/sendtoEmail", (req, res) => {
     },
   });
 
+  app.get("/getAdminInformations_/:contact_id", (req, res) => {
+    const contact_id = req.params.contact_id;
+    console.log(contact_id);
+    const sqlGet = "SELECT * FROM contact_us WHERE contact_id = ?";
+
+    db.query(sqlGet, contact_id, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(result);
+        res.send(result);
+      }
+    });
+  });
+
   const mailOptions = {
     from: "perseeption2021@gmail.com",
     to: EmailContact,
