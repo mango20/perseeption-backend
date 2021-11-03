357447b2bdadb26c47246bb2e10f2648e4b5e26a
unknown <mangacharmainelilan20@gmail.com>
2021-11-03 13:56:37 +0800

    make it better

--- index.js
+++ index.js
@@ -58,7 +58,11 @@ app.use(
 
 // app.use(cors(corsOptions));
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
+  connectionLimit: 1000,
+  connectTimeout: 60 * 60 * 1000,
+  acquireTimeout: 60 * 60 * 1000,
+  timeout: 60 * 60 * 1000,
   host: "us-cdbr-east-04.cleardb.com",
   port: 3306,
   user: "bc62b0ccf843e4",
