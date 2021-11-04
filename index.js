450c9bbc2942b6a4219ac6a29ac7212ec6272411
unknown <mangacharmainelilan20@gmail.com>
2021-11-04 23:24:55 +0800

    make it better

--- index.js
+++ index.js
@@ -136,28 +136,21 @@ app.post("/api/upload", async (req, res) => {
     const EVENT_TITLE = req.body.EVENT_TITLE;
     const EVENT_CONTENT = req.body.EVENT_CONTENT;
     const fileStr = req.body.data;
+    const sql =
+      "INSERT INTO admin_events (EVENT_TITLE,EVENT_CONTENT, EVENT_IMAGE ) VALUES (?,?,?)";
+    db.query(sql, EVENT_TITLE, EVENT_CONTENT, fileStr, (err, results) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(results);
+      }
+    });
     const uploadedResponse = await cloudinary.uploader.upload(fileStr, {
       upload_preset: "jogvnb1m",
     });
     console.log(uploadedResponse);
     res.send({ mgs: "ehgfhsgehfe" });
     console.log(fileStr);
-
-    const sql =
-      "INSERT INTO admin_events (EVENT_TITLE,EVENT_CONTENT, EVENT_IMAGE ) VALUES (?,?,?)";
-    db.query(
-      sql,
-      EVENT_TITLE,
-      EVENT_CONTENT,
-      uploadedResponse,
-      (err, results) => {
-        if (err) {
-          console.log(err);
-        } else {
-          res.send(results);
-        }
-      }
-    );
   } catch (error) {
     console.log(error);
   }
