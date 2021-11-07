31ce3a5253f3185ae534b3da7222e6f59347e9cc
unknown <mangacharmainelilan20@gmail.com>
2021-11-07 23:10:22 +0800

    make it better

--- index.js
+++ index.js
@@ -36,10 +36,17 @@ const fileupload = require("express-fileupload");
 //   password: "We1*|Ber#J!<",
 //   database: "perseeption_db-36352871",
 // });
-// app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
+app.use(express.static("public"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+// app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "PUT", "HEAD", "OPTIONS"],
+    credentials: true,
+  })
+);
 
 // app.all("*", function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
@@ -184,7 +191,7 @@ app.post("/api/upload", async (req, res) => {
     const uploadedResponse = await cloudinary.uploader.upload(fileStr, {
       upload_preset: "jogvnb1m",
     });
-    console.log(uploadedResponse);
+    // console.log(uploadedResponse);
     const url = uploadedResponse.public_id;
     res.send({ mgs: "ehgfhsgehfe" });
     const sql =
