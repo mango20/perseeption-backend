6c4c6001a1db50ed9ca109c07f7fc1ead82840cd
unknown <mangacharmainelilan20@gmail.com>
2021-11-07 20:42:38 +0800

    make it better

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const fileupload = require("express-fileupload");
 //   password: "We1*|Ber#J!<",
 //   database: "perseeption_db-36352871",
 // });
-
+app.use(express.static("public"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 // app.use(cors());
