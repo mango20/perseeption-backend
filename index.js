658c5c84712d72e3c62ca81016bc6ba52ff594a0
unknown <mangacharmainelilan20@gmail.com>
2021-11-07 20:54:13 +0800

    make it better

--- index.js
+++ index.js
@@ -39,14 +39,14 @@ const fileupload = require("express-fileupload");
 app.use(express.static("public"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors());
-// app.use(
-//   cors({
-//     origin: true,
-//     methods: ["GET", "POST", "DELETE", "PUT"],
-//     credentials: true,
-//   })
-// );
+// app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 
 // app.all("*", function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
